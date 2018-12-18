19cbd09edcac6d68c35f213955950611370d5aaa
thiago619 <jurassicpark171@gmail.com>
2018-12-18 21:45:07 -0200

    Correçao do bug do movimento

---
+++ Linha Azul/Assets/Scripts/player/CaracterBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CaracterBase : MonoBehaviour {
+
+    //Características básicas dos personagens
+
+    // Use this for initialization
+
+    public float HpMaximo;
+    public float MpMaximo;
+    public float forca;
+    public float magica;
+    public float defesaBase;
+    public float defesaMagicaBase;
+    public float hit;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Linha Azul/Assets/Scripts/player/PlayerMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script responsável por mover o personagem
+
+public class PlayerMove : MonoBehaviour {
+
+
+    private Vector2 direcao;
+    public int velocidade;
+
+	// Use this for initialization
+	void Start () {
+        direcao = Vector2.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        direcaotoMove();
+
+         transform.Translate(direcao * velocidade * Time.deltaTime);
+	}
+
+    private void direcaotoMove()
+    {
+        direcao = Vector2.zero;
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            direcao = Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direcao = Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direcao = Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direcao = Vector2.right;
+        }
+
+    }
+}
